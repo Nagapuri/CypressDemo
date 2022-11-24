8947d62f7ef4e02f552a50d92f67328a2bc21a8f
RamaKrishna <ram.946@gmail.com>
2022-11-24 14:20:50 +0530

    modified pakage.json and cypress.connfig.js .gitig

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,16 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  chromeWebSecurity: false,
+  projectId: 'ioceso',
+  screenshotOnRunFailure: true,
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'cypress/report',
+    charts: true,
+    reportPageTitle: 'Joan Media Demo Report',
+    embeddedScreenshots: true,
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
