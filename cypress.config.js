062741d3f441fbaa0a0190d9f0bcd2e1d78a2ac3
RamaKrishna <ram.946@gmail.com>
2022-11-24 14:43:05 +0530

    mocha changes

--- cypress.config.js
+++ cypress.config.js
@@ -4,13 +4,14 @@ module.exports = defineConfig({
   chromeWebSecurity: false,
   projectId: 'ioceso',
   screenshotOnRunFailure: true,
-  reporter: 'cypress-mochawesome-reporter',
-  reporterOptions: {
-    reportDir: 'cypress/report',
-    charts: true,
-    reportPageTitle: 'Joan Media Demo Report',
-    embeddedScreenshots: true,
-  },
+  reporter: 'mochawesome',
+  // reporterOptions: {
+  //   reportDir: 'cypress/report',
+  //   html: false,
+  //   charts: true,
+  //   reportPageTitle: 'Joan Media Demo Report',
+  //   embeddedScreenshots: true,
+  // },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
